1e8578a5b60613a10f178b1e4700bd6116911cd8
imp <yanpeng@codoon.com>
2018-02-12 17:52:54 +0800

    TouchID：first commit

--- TouchID/ViewController.m
+++ TouchID/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <LocalAuthentication/LocalAuthentication.h>
 
 @interface ViewController ()
+
+@property (nonatomic, strong) LAContext *context;
 
 @end
 
@@ -16,9 +19,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    _context = [[LAContext alloc] init];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)buttonAction:(id)sender {
+    [_context evaluatePolicy:LAPolicyDeviceOwnerAuthentication localizedReason:@"开启touchID？" reply:^(BOOL success, NSError * _Nullable error) {
+        if (success) {
+            NSLog(@"success");
+        }else{
+            NSLog(@"%@",error);
+        }
+    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
